3d5f81e8fb161015d1b37fc83b82d690c4eb6dc5
Utku Can Yılmaz <utkucanyilmaz26@gmail.com>
2023-04-06 23:32:46 +0300

    Add Session component

---
+++ src/components/Session.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import "./Session.css";
+
+function Session() {
+  return (
+    <div className="session">
+      <div className="session-name">PRACTICE 1</div>
+      <div className="session-day">FRI</div>
+      <div className="session-time">13:30</div>
+    </div>
+  );
+}
+
+export default Session;
