35abe044725c7ef97cd6475a5546fce5ec98b157
Utku Can Yılmaz <utkucanyilmaz26@gmail.com>
2023-04-08 23:08:15 +0300

    Add fetching data from APIs, replace placeholder data

--- src/components/Race.jsx
+++ src/components/Race.jsx
@@ -1,45 +1,94 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import SessionsTable from "./SessionsTable";
+import Countdown from "./Countdown";
 import "./Race.css";
 
 function Race() {
+  const [upcomingRace, setUpcomingRace] = useState(null);
+  const [flag, setFlag] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+
+  async function getUpcomingRaceData() {
+    const now = new Date().getTime();
+
+    try {
+      const raceDataResponse = await fetch(
+        "https://ergast.com/api/f1/current.json"
+      );
+      const {
+        MRData: {
+          RaceTable: { Races },
+        },
+      } = await raceDataResponse.json();
+
+      const raceInfo = await Races.find(
+        race => new Date(race.date).getTime() > now
+      );
+
+      const flagResponse = await fetch(
+        `https://restcountries.com/v3.1/name/${raceInfo.Circuit.Location.country}?fields=flags`
+      );
+
+      const [{ flags }] = await flagResponse.json();
+
+      const firstRaceDay = new Date(raceInfo.FirstPractice.date).toLocaleString(
+        "en-GB",
+        { day: "numeric", month: "short" }
+      );
+
+      const lastRaceDay = new Date(raceInfo.date).toLocaleString("en-GB", {
+        day: "numeric",
+        month: "short",
+      });
+
+      setUpcomingRace({ raceInfo, firstRaceDay, lastRaceDay });
+      setFlag(flags);
+      setIsLoading(false);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  useEffect(() => {
+    getUpcomingRaceData();
+  }, []);
+
   return (
     <div className="race">
-      <div className="race-round">Round 4 - Upcoming Race</div>
-      <div className="race-info">
-        <div className="race-country-image">
-          <img
-            src="https://upload.wikimedia.org/wikipedia/commons/d/dd/Flag_of_Azerbaijan.svg"
-            alt="flag"
-          />
-        </div>
-        <div>
-          <div className="race-info-year">2023</div>
-          <div className="race-info-title">Azerbaijan Grand Prix</div>
-          <div className="race-info-date">
-            28-30
-            <span className="race-info-month">APR</span>
+      {!isLoading && (
+        <>
+          <div className="race-round">
+            Round {upcomingRace.raceInfo.round} - Upcoming Race
           </div>
-        </div>
-      </div>
-      <div className="race-info-countdown">
-        <span className="number">
-          30 <span className="type">Days</span>
-        </span>
-        <span className="number">
-          30 <span className="type">Hours</span>
-        </span>
-        <span className="number">
-          30 <span className="type">Mins</span>
-        </span>
-        <span className="number">
-          30 <span className="type">Seconds</span>
-        </span>
-      </div>
-      <SessionsTable />
-      <div className="times-displayed">
-        Times displayed are your local times.
-      </div>
+          <div className="race-info">
+            <div className="race-country-image">
+              <img
+                src={flag.svg}
+                alt={`${upcomingRace.raceInfo.Circuit.Location.country} flag`}
+              />
+            </div>
+            <div>
+              <div className="race-info-year">
+                {upcomingRace.raceInfo.season}
+              </div>
+              <div className="race-info-title">
+                {upcomingRace.raceInfo.raceName}
+              </div>
+              <div className="race-info-date">
+                {`${upcomingRace.firstRaceDay} - ${upcomingRace.lastRaceDay}`}
+              </div>
+            </div>
+          </div>
+          <Countdown
+            date={upcomingRace.raceInfo.date}
+            time={upcomingRace.raceInfo.time}
+          />
+          <SessionsTable />
+          <div className="times-displayed">
+            Times displayed are your local times.
+          </div>
+        </>
+      )}
     </div>
   );
 }
