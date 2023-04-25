4d17e79b8e21f88b06cac3ca75bbde21ca722b4e
Utku Can Yılmaz <utkucanyilmaz26@gmail.com>
2023-04-25 23:28:32 +0300

    Add fetchDriverStandings function to api.js

--- src/api.js
+++ src/api.js
@@ -8,7 +8,26 @@ export const fetchRaceData = async () => {
         RaceTable: { Races },
       },
     } = await raceDataResponse.json();
+
     return Races;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const fetchDriverStandings = async () => {
+  try {
+    const driverStandings = await fetch(
+      "https://ergast.com/api/f1/current/driverStandings.json"
+    );
+
+    const {
+      MRData: {
+        StandingsTable: { StandingsLists },
+      },
+    } = await driverStandings.json();
+
+    return StandingsLists[0].DriverStandings;
   } catch (error) {
     console.log(error);
   }
