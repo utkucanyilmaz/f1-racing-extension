3564e86da931f5521fd56c7a3159cdf4ac3f539f
Utku Can Yılmaz <utkucanyilmaz26@gmail.com>
2023-04-26 23:55:19 +0300

    Add getDriverStandings function to App.jsx, Minor change on Calendar.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,33 @@
 import React, { useState, useEffect } from "react";
 import Race from "./components/race-components/Race";
 import Calendar from "./components/calendar-components/Calendar";
-import { fetchRaceData } from "./api.js";
+import StandingsTable from "./components/standing-components/StandingsTable";
+import { fetchRaceData, fetchDriverStandings } from "./api";
 
 function App() {
   const [races, setRaces] = useState(null);
+  const [driverStandings, setDriverStandings] = useState(null);
 
   useEffect(() => {
-    const getData = async () => {
-      const data = await fetchRaceData();
-      setRaces(data);
+    const getRaces = async () => {
+      const racesData = await fetchRaceData();
+      setRaces(racesData);
     };
-    getData();
+
+    const getDriverStandings = async () => {
+      const driverStandingsData = await fetchDriverStandings();
+      setDriverStandings(driverStandingsData);
+    };
+
+    getRaces();
+    getDriverStandings();
   }, []);
 
   return (
     <div className="App">
       <Race races={races} />
       {/* <Calendar races={races} /> */}
+      {/* <StandingsTable driverStandings={driverStandings} /> */}
     </div>
   );
 }
--- src/components/calendar-components/Calendar.jsx
+++ src/components/calendar-components/Calendar.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React from "react";
 import CalendarItem from "./CalendarItem";
 import "./Calendar.css";
 
