c8f45d3def415ba8e64d407f1ff85c9a38d0b960
Utku Can Yılmaz <utkucanyilmaz26@gmail.com>
2023-06-23 00:17:06 +0300

    Update RaceInfo and SessionTable stylings

--- src/components/race-components/RaceInfo.jsx
+++ src/components/race-components/RaceInfo.jsx
@@ -6,26 +6,30 @@ function RaceInfo({ upcomingRace, flag }) {
   return (
     <div className="race-info">
       <div className="race-info-wrapper">
-        {/* <div className="race-round">
-          Round {upcomingRace.raceInfo.round} - Upcoming Race
-        </div> */}
-        <div className="race-country-image">
-          {flag ? (
-            <img
-              src={flag.svg}
-              alt={`${upcomingRace.raceInfo.Circuit.Location.country} flag`}
-            />
-          ) : (
-            <div className="race-country-image-placeholder"></div>
-          )}
-        </div>
-        <div>
-          <div className="race-info-year">{upcomingRace.raceInfo.season}</div>
-          <div className="race-info-title">
-            {upcomingRace.raceInfo.raceName}
-          </div>
-          <div className="race-info-date">
+        <div className="race-year">{upcomingRace.raceInfo.season}</div>
+        <div className="race-title">{upcomingRace.raceInfo.raceName}</div>
+        <div className="race-round-date">
+          <div className="race-round">Round {upcomingRace.raceInfo.round}</div>
+          <div className="race-date">
             {`${upcomingRace.firstRaceDay} - ${upcomingRace.lastRaceDay}`}
+          </div>
+        </div>
+        <div className="race-location">
+          <div className="race-country-image">
+            {flag ? (
+              <img
+                src={flag.svg}
+                alt={`${upcomingRace.raceInfo.Circuit.Location.country} flag`}
+              />
+            ) : (
+              <div className="race-country-image-placeholder"></div>
+            )}
+          </div>
+          <div className="race-country">
+            {upcomingRace.raceInfo.Circuit.Location.country} -{" "}
+            <span className="circuit-name">
+              {upcomingRace.raceInfo.Circuit.circuitName}
+            </span>
           </div>
         </div>
       </div>
