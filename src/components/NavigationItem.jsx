03d5a9cb429e66ee6b231fd60aa3e5e2f01bc097
Utku Can Yılmaz <utkucanyilmaz26@gmail.com>
2023-06-28 15:39:27 +0300

    Update styling on NavigationItem and CalendarItem

--- src/components/NavigationItem.jsx
+++ src/components/NavigationItem.jsx
@@ -4,7 +4,7 @@ function NavigationItem({ isActive, children, onItemClick, className }) {
   return (
     <button
       onClick={onItemClick}
-      className={`py-2 px-4 border-t rounded-full ${
+      className={`py-2 px-4 border-t rounded-xl ${
         isActive
           ? "border-f-red"
           : "border-f-light-gray rounded-full text-f-light-gray hover:text-white hover:border-white transition-colors cursor-pointer"
