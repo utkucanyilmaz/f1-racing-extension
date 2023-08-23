de327c44b1d77b966f4b90b8373784bf9c86883a
Utku Can Yılmaz <utkucanyilmaz26@gmail.com>
2023-08-23 18:21:03 +0300

    Updated LoadingPlaceholder dimensions to use rem unit

--- src/components/race-components/LoadingPlaceholder.jsx
+++ src/components/race-components/LoadingPlaceholder.jsx
@@ -6,20 +6,20 @@ function LoadingPlaceholder() {
     <>
       <div className="flex flex-col gap-1">
         <Skeleton
-          width={273}
-          height={126}
+          width={"19.5rem"}
+          height={"9rem"}
           className={"rounded-b-xl rounded-t-none"}
         />
         <div className="flex gap-x-4">
-          <Skeleton width={120} height={32} />
-          <Skeleton width={100} height={32} />
+          <Skeleton width={"8rem"} height={"2.3rem"} />
+          <Skeleton width={"6.6rem"} height={"2.3rem"} />
         </div>
         <div className="flex gap-x-1">
-          <Skeleton width={88} height={56} />
-          <Skeleton width={88} height={56} />
-          <Skeleton width={88} height={56} />
+          <Skeleton width={"6.3rem"} height={"4rem"} />
+          <Skeleton width={"6.3rem"} height={"4rem"} />
+          <Skeleton width={"6.3rem"} height={"4rem"} />
         </div>
-        <Skeleton width={273} height={246} />
+        <Skeleton width={"19.5rem"} height={"17.45rem"} />
       </div>
     </>
   );
